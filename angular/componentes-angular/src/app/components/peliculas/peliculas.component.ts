e3e012f19ee313e49e102c884ec434d94bd36734
Alex Mancinas <alexman381@gmail.com>
2022-06-23 23:28:40 -0700

    avance

--- angular/componentes-angular/src/app/components/peliculas/peliculas.component.ts
+++ angular/componentes-angular/src/app/components/peliculas/peliculas.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
   title: string;
+  public peliculas: Array<any  >;
+
   constructor() { 
     this.title = "Componente peliculas"
-
-
-
-    console.log("Constructor lanzado");
+    this.peliculas =[
+      {year: 2019 ,title: 'Spiderman 4', image: 'https://cdn.pocket-lint.com/r/s/970x/assets/images/159643-tv-news-spider-man-no-way-home-image1-dryautoefj-jpg.webp'},
+      {year: 2019 ,title: 'Avengers: end game', image: 'https://ichef.bbci.co.uk/news/800/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg.webp'},
+      {year: 2015 ,title: 'Batman VS Superman', image: 'https://www.elviejotopo.com/wp-content/uploads/2016/03/batman-v-superman-dawn-of-justice_bb788b6f.jpg'}
+    ]
 
 
   }
