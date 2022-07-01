ad9b6b4b93d8aaf83eb0745d1e0558a1592dc993
Alex Mancinas <alexman381@gmail.com>
2022-06-30 17:58:39 -0700

    nada

--- angular/todolist/src/app/app.component.ts
+++ angular/todolist/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist';
+  
+  public tareas!: Array<string>
+  tarea!: string 
+  constructor(){
+    
+  }
+
 }
---
+++ angular/todolist/src/app/tarea/tarea.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.scss']
+})
+export class TareaComponent implements OnInit {
+  
+  
+
+    
+  constructor(
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
